a0ae4bbe645ae5e6209c185ce0d35b6858c61f3d
Socrates sotos <socrates.sotos@gmail.com>
2016-09-20 18:40:15 -0400

    Uploaded Full Project
    -Added entire project

---
+++ Music Generator/Assets/Scripts/AudioSample.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class AudioSample {
+
+	public AudioClip clip;
+	public float startTime;
+	public float currentWriteTime;
+
+	public AudioSample (AudioClip _clip, float _startTime) {
+
+		clip = _clip;
+		startTime =_startTime;
+		currentWriteTime = 0.0f;
+
+	}
+
+	public bool HasBeenComposited () {
+
+		return currentWriteTime >= clip.length;
+
+	}
+
+	public void UpdateCurrentWriteTime (float time) {
+		currentWriteTime += time;
+	}
+
+}
---
+++ Music Generator/Assets/Scripts/Score.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Score : MonoBehaviour {
+
+	public Track track;
+
+	public float tempo = 120.0f;
+	public int signatureHi = 4;
+	public int signatureLo = 4;
+	public int subdivisions = 1;
+
+	private float subdivisionLength;
+	private int playheadPosition = -1;
+	private float nextBeat;
+
+	[HideInInspector] public List<AudioSample> samples = new List<AudioSample> ();
+
+	public AudioClip kick;
+	public AudioClip snare;
+	public AudioClip pad;
+
+	// Use this for initialization
+	void Start () {
+
+		subdivisionLength = 60.0f / (tempo * subdivisions);
+		nextBeat = 0;
+	
+	}	
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Time.time >= nextBeat) {
+
+			DetermineNextBeat ();
+			nextBeat += subdivisionLength;
+
+			//print (++playheadPosition);
+			playheadPosition++;
+			if (playheadPosition == signatureHi) playheadPosition = 0;
+
+		}
+	
+	}
+
+	void DetermineNextBeat () {
+
+		samples.Add (new AudioSample (kick, nextBeat + subdivisionLength));
+
+		for (int i = 0; i < snareHits.Count; i++) {
+			if (playheadPosition == snareHits[i]) {
+
+				samples.Add (new AudioSample (snare, nextBeat + subdivisionLength));
+
+			}
+
+		}
+
+		track.WriteNextAudioFrame (samples, nextBeat - subdivisionLength, subdivisionLength);
+
+	}
+
+	private List<int> snareHits = new List<int> ();
+
+	public void GenerateNewBeat () {
+
+		tempo = Random.Range (80f, 160f);
+		subdivisionLength = 60.0f / (tempo * subdivisions);
+		signatureHi = Random.Range (3, 10);
+
+		snareHits.Clear ();
+
+		int numberOfSnares = Random.Range (2, signatureHi);
+
+		for (int i = 0; i < numberOfSnares; i++) {
+
+			snareHits.Add (Random.Range (1, signatureHi - 1));
+
+		}
+
+
+
+	}
+
+}
